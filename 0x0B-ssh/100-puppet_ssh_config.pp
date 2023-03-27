187d635539d92bd6a7cb9fff4066b08155cda106
bentalla108 <tallambaye108@gmail.com>
2023-03-26 17:50:54 -0700

    Puppet config to ensure auth password is off

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+include stdlib
+#Check ssh config file 
+
+file_line { 'Turn off password auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}

eb2c3d3b4cb94974676fba4eaa52bc1778f5d8ff
bentalla108 <tallambaye108@gmail.com>
2023-04-24 22:54:06 +0000

    Done after 50min of buging my self with bad name ofscript

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that use https://jsonplaceholder.typicode.com/ to return
+a todo list progress for a giveen employee
+"""
+
+import re
+import requests as r
+import sys
+
+url = "https://jsonplaceholder.typicode.com"
+"""URL for API"""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            usr_req = r.get('{}/users/{}'.format(url, id)).json()
+            all_todos = r.get('{}/todos'.format(url)).json()
+            usr_name = usr_req.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, all_todos))
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            usr_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )

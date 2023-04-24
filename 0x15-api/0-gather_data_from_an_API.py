2ea2ae694eaf88f2b2d34a46cd2b16bd8a09b8a5
bentalla108 <tallambaye108@gmail.com>
2023-04-24 21:58:04 +0000

    API REST

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that use https://jsonplaceholder.typicode.com/ to return 
+a todo list progress for a giveen employee
+"""
+
+import re
+import requests as r
+import sys
+
+url = "https://jsonplaceholder.typicode.com"
+"""URL for API"""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            usr_req = r.get('{}/users/{}'.format(url,id)).json()
+            all_todos = r.get('{}/todos'.format(url)).json()
+            usr_name = usr_req.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, all_todos))
+            todos_done =  list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    usr_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
